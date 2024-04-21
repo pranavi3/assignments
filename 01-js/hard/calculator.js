b905a4db118c67d55b4cd6b5d143b9e7911234ea
Pranavi <pranavi.nukala0302@gmail.com>
2024-04-21 18:54:21 +0530

    01-js calculator

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -15,34 +15,114 @@
 
   Once you've implemented the logic, test your code by running
 */
-
 class Calculator {
-  constructor(result){
-    this.result = 0;
+  constructor() {
+      this.result = 0;
   }
+
   add(val) {
-    this.result = this.result + val;
+      this.result += val;
   }
-  subtract(val){
-    this.result = this.result - val;
+
+  subtract(val) {
+      this.result -= val;
   }
-  multiply(val){
-    this.result = this.result * val;
+
+  multiply(val) {
+      this.result *= val;
   }
-  divide(val){
-    if(val == 0){throw new Error();}
-    this.result = this.result / val;
+
+  divide(val) {
+      if (val === 0) {
+          throw new Error('Division by zero');
+      }
+      this.result /= val;
   }
-  clear(){
-    this.result = 0;
+
+  clear() {
+      this.result = 0;
   }
-  getResult(){
-    return this.result;
+
+  getResult() {
+      return this.result;
   }
-  calculate(str){
-    let stack = [];
-    return 0;
+
+  calculate(str) {
+      let val = [];
+      let ops = [];
+      let currNum = '';
+      let open = 0;
+      for (let i = 0; i < str.length; i++) {
+          if (str[i] === ' ') {
+              continue;
+          } else if (str[i] === '(') {
+              open++;
+              ops.push(str[i]);
+          } else if (str[i] === ')') {
+              if(open==0){
+                throw new Error();
+              }
+              while (ops.length !== 0 && ops[ops.length - 1] !== '(') {
+                  let v2 = val.pop();
+                  let v1 = val.pop();
+                  let operation = ops.pop();
+                  val.push(this.apply(v1, v2, operation));
+              }
+              if (ops.length === 0) {
+                  throw new Error('Invalid parenthesis');
+              }
+              ops.pop(); // Remove '('
+              open--;
+          } else if (str[i] === '+' || str[i] === '-' || str[i] === '*' || str[i] === '/') {
+              while (ops.length !== 0 && this.precedence(ops[ops.length - 1]) >= this.precedence(str[i])) {
+                  let v2 = val.pop();
+                  let v1 = val.pop();
+                  val.push(this.apply(v1, v2, ops.pop()));
+              }
+              ops.push(str[i]);
+              currNum = ''; // Reset current number
+          } else if (!isNaN(parseInt(str[i])) || str[i] === '.') {
+              currNum += str[i];
+              if (i === str.length - 1 || isNaN(parseInt(str[i + 1])) && str[i + 1] !== '.') {
+                  val.push(parseFloat(currNum));
+                  currNum = ''; // Reset current number
+              }
+          } else {
+              throw new Error('Invalid character in expression');
+          }
+      }
+      if(open!=0){
+        throw new Error();
+      }
+      while (ops.length !== 0) {
+          let v2 = val.pop();
+          let v1 = val.pop();
+          val.push(this.apply(v1, v2, ops.pop()));
+      }
+      
+      if (val.length !== 1 || ops.length !== 0) {
+          throw new Error('Invalid expression');
+      }
+      this.result = val[0];
+  }
+
+  precedence(operator) {
+      if (operator === '+' || operator === '-') return 1;
+      else if (operator === '*' || operator === '/') return 2;
+      return 0;
+  }
+
+  apply(v1, v2, operation) {
+      switch (operation) {
+          case '+': return v1 + v2;
+          case '-': return v1 - v2;
+          case '*': return v1 * v2;
+          case '/': 
+            if(v2 == 0)throw new Error();
+            return v1/v2;
+      }
   }
 }
 
 module.exports = Calculator;
+
