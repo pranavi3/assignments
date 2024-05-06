e0347dd8fb9d54791da9235de14a23bd9fbdc332
Pranavi <pranavi.nukala0302@gmail.com>
2024-05-06 10:18:33 +0530

    promises-chain hard

--- week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,33 @@
  */
 
 function wait1(t) {
-
+    return new Promise(resolve => {
+        setTimeout(resolve, t*1000);
+      });
 }
 
 function wait2(t) {
-
+    return new Promise(resolve => {
+        setTimeout(resolve, t*1000);
+      });
 }
 
 function wait3(t) {
-
+    return new Promise(resolve => {
+        setTimeout(resolve, t*1000);
+      });
 }
 
 function calculateTime(t1, t2, t3) {
-
+    let time = new Date().getTime();
+    return wait1(t1).then(()=>{
+        return wait2(t2).then(()=>{
+            return wait3(t3).then(()=>{
+                let end = new Date().getTime();
+                return (end-time);
+            })
+        })
+    })
 }
 
 module.exports = calculateTime;
